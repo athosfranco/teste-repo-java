21a55c64eaba12101afab283764a1aac2ad4fe29
Athos Franco <athos.francof@gmail.com>
2021-11-26 19:45:22 -0300

    modificadores de acesso

--- curso-java-web/src/PrimeiraClasseJava.java
+++ curso-java-web/src/PrimeiraClasseJava.java
@@ -1,12 +1,12 @@
-
+import classes.objetos.Aluno;
 
 public class PrimeiraClasseJava {
 
 	public static void main(String[] args) {
 
-		double numeroDouble = 2;
-		int numeroInt = (int) numeroDouble;
-		System.out.println(numeroInt);
+		Aluno aluno1 = new Aluno("Athos", "Franco", "02/09/1995", "17/11/2021", "24092394", "607.198.163-86", "Márcio Passos Feitosa", "Marcenia Monroe Franco de Sá", 26);
+		
+		aluno1.imprimirDados();
 
 	}
 
