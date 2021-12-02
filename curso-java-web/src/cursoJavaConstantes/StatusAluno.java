bf915731dfc1a4718b64e6a144c2a18753096ad7
Athos Franco <athos.francof@gmail.com>
2021-12-01 22:07:46 -0300

    classes e atributos estaticos

---
+++ curso-java-web/src/cursoJavaConstantes/StatusAluno.java
@@ -0,0 +1,9 @@
+package cursoJavaConstantes;
+
+public class StatusAluno {
+	public static String APROVADO = "Aprovado";
+	public static String REPROVADO = "Reprovado";
+	public static String RECUPERACAO = "Recuperação";
+	
+
+}
