91f97a2f8372f092269934f750dcc1b55123eb6e
Athos Franco <athos.francof@gmail.com>
2021-12-07 08:22:45 -0300

    aula principio responsb

---
+++ curso-java-web/src/principioResponsabilidade/contaBancaria.java
@@ -0,0 +1,13 @@
+package principioResponsabilidade;
+
+public class contaBancaria {
+	
+	private String descricao;
+	
+	private double saldo = 8000;
+	
+	public void soma100Reais() {
+		this.saldo += 100;
+	}
+
+}
